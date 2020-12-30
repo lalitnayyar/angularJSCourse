3e44a609c14feb2f3633b810b97c7c94a12ecfca
Lalit Nayyar <lalitnayyar@gmail.com>
2020-12-30 18:03:31 +0400

    47 adding two way binding
    47 adding two way binding

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -21,9 +21,19 @@ class App extends Component {
         { name: newName, age: 28 },
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 25 },
-      ],
+      ]
     });
   };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 26 },
+      ]
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -37,7 +47,8 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-         click={this.switchNameHandler.bind(this,'Maxi!')}>
+         click={this.switchNameHandler.bind(this,'Maxi!')}
+         changed={this.nameChangedHandler}>
           {" "}
           My hobbies : Racing{" "}
         </Person>
