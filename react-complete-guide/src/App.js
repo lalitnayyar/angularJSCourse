7e7c0ac0812ba1c2e7cb33c9391d03c04fd60990
Lalit Nayyar <lalitnayyar@gmail.com>
2021-01-05 14:59:44 +0400

    69 using radium for media queries
    69 using radium for media queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 //import logo from './logo.svg';
 import "./App.css";
-import Radium from 'radium';
-
+//import Radium from 'radium';
+import Radium, { StyleRoot } from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -50,10 +50,10 @@ class App extends Component {
       border: "1x solid yellow",
       padding: "8px",
       cursor: "pointer",
-      ':hover':{
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
     };
 
     let persons = null;
@@ -75,29 +75,30 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
-      style[':hover']={
-        backgroundColor: 'lightred',
-        color: 'black'
-      }
+      style[":hover"] = {
+        backgroundColor: "lightred",
+        color: "black",
+      };
     }
-    const classes = [ ];
-    if (this.state.persons.length <= 2){
-classes.push('red') // classes = ['red']
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red"); // classes = ['red']
     }
-    if (this.state.persons.length<=1){
-      classes.push('bold') // classes = ['bold']
+    if (this.state.persons.length <= 1) {
+      classes.push("bold"); // classes = ['bold']
     }
-    
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1> I am react app </h1>
-        <p className={classes.join(' ')}>This is really working !!!</p>
+        <p className={classes.join(" ")}>This is really working !!!</p>
         <button style={style} onClick={this.togglePersonHandler}>
           Toggle Person
         </button>
         {persons}
       </div>
+      </StyleRoot>
     );
     //return React.createElement('div',{className: 'App'},React.createElement('h1',null,' Hi, I\'m a React App !!!'));
   }
